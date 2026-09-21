967a1db3fa31290df28d917a47294e8112f55ecd
Jaber <jaberc69@gmail.com>
2026-09-21 20:44:35 -0300

    Sprint 1 concluído.

--- js/product.js
+++ js/product.js
@@ -7,25 +7,33 @@ const buscaDaUrl = window.location.search;
 // Método para transformar a string em um int
 const parametros = new URLSearchParams(buscaDaUrl);
 //Vai atribuir à variável idDoLivro o resultado de get.
-const idDoLivro = parametros.get('id');
+const idDoLivro = parametros.get("id");
 // Vai procurar a id do Livro.
-const livroSelecionado = dados.livros.find(function(livro){
-    return livro.id === idDoLivro;
+const livroSelecionado = dados.livros.find(function (livro) {
+  return livro.id === idDoLivro;
 });
-
 
 const areaDetalhes = document.querySelector(".book-detail");
 if (!livroSelecionado) {
-    areaDetalhes.innerHTML = `
+  areaDetalhes.innerHTML = `
     <h1>Livro não encontrado</h1>
     <a href="./index.html#catalogo">Voltar ao catálogo</a>
     `;
 } else {
-    areaDetalhes.innerHTML = `
-    <h2>${livroSelecionado.titulo}</h2>
-    <p>Por ${livroSelecionado.autor}</p>
-    <span>${precoAjustado.toFixed(2)}</span>
-    <p>${livroSelecionado.descricao}</p>
-    <button class="book-detail__add">Adicionar ao carrinho</button>
+  areaDetalhes.innerHTML = `
+<div class="book-detail__cover book-cover book-cover--coral">
+  <h1>${livroSelecionado.titulo}</h1>
+  <small>${livroSelecionado.autor}</small>
+  <b>${livroSelecionado.paginas}</b>
+</div>
+<div class="book-detail__content">
+  <h2>${livroSelecionado.titulo}</h2>
+  <p class="book-detail__author">por ${livroSelecionado.autor}</p>
+  <strong class="book-detail__price">${formatarPreco(livroSelecionado.preco)}</strong>
+  <p class="book-detail__description">${livroSelecionado.descricao}</p>
+  <div class="book-detail__buy">
+    <button class="book-detail__add" type="button">Adicionar ao carrinho</button>
+  </div>
+</div>
     `;
 }
